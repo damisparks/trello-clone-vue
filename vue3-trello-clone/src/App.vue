e2709b9d91a0a1306f3efa5fef7d93d60e8bddd4
Damilola Omifare <omifaredammy@gmail.com>
2023-07-21 12:10:34 +0100

    refactor: restructure folder

--- vue3-trello-clone/src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-</script>
-
-<template>
-  <div>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione sit illum culpa, voluptatum suscipit sed dicta incidunt delectus adipisci modi eos debitis rem, facilis excepturi cupiditate sapiente voluptate eaque ex.
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
